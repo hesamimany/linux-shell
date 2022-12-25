0ca86d8fa4bc6afa42e7787a449be79694848f80
Ghazale-Chakane <82727653+Ghazale-Chakane@users.noreply.github.com>
2022-12-25 16:52:41 +0330

    Update main.c
    adding firstWord and highRepeat

--- main.c
+++ main.c
@@ -28,11 +28,67 @@ void parseSpace(char* str, char** parsed){
 	}
 }
 
-void firstWord(){
-
+void firstWord(char* add){
+	FILE *fp;
+	char word[30];
+	fp = fopen(add, "r");
+	if (fp == NULL) {
+    	printf("File not found!\n");
+	} else {
+    	while (!feof(fp)) {
+        	fscanf(fp,"%s%*[^\n]",word);
+        	printf("%s\n", word);
+        	strcpy(word,"");
+    	}
+	}
+	fclose(fp);
 }
-void highRepeat(){
+void highRepeat(char *add){
+	FILE *file;
+	char ch, *line;
+	size_t len = 0, read;
+	char words[1000][1000], word[20];
+	int i = 0, j, k, maxCount = 0, count;
+	file = fopen(add,"r");
 
+	if (file == NULL){
+		printf("File not found");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, file)) != -1) {
+
+		for(k=0; line[k]!='\0'; k++){
+			
+			if(line[k] != ' ' && line[k] != '\n' && line[k] != ',' && line[k] != '.' ){
+				words[i][j++] = tolower(line[k]);
+			}
+			else{
+				words[i][j] = '\0';
+				
+				i++;
+			
+				j = 0;
+			}
+		}
+	}
+
+	int length = i;
+	for(i = 0; i < length; i++){
+		count = 1;
+		for(j = i+1; j < length; j++){
+			if(strcmp(words[i], words[j]) == 0 && (strcmp(words[i]," ") != 0)){
+				count++;
+			}
+		}
+		if(count > maxCount){
+			maxCount = count;
+			strcpy(word, words[i]);
+		}
+	}
+
+	printf("Most repeated word: %s", word);
+	fclose(file);
 }
 void rmSpace(){
 
